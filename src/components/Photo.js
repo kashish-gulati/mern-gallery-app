679697141c341ed68acd5feac5f8b4a57f0eb2df
Kashish Gulati <kashlati@gmail.com>
2021-08-02 15:44:13 +0530

    initial commit

---
+++ src/components/Photo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+const Photo = ({id}) => (
+    <Card className="photo">
+        <Card.Img
+            variant = "top"
+            src = {`http://localhost:3300/photos/${id}`}
+            alt="Photo"
+        />
+    </Card>
+);
+
+export default Photo;
